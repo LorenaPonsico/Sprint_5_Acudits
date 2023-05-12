15011c13fff0d0aa2b6c0167ad6cfea96df9eb9b
Lorena Ponsico <lorena.poca@gmail.com>
2023-05-12 12:06:38 +0200

    n1 ex1 mostrar per consola acudit

---
+++ main.ts
@@ -0,0 +1,41 @@
+const API_URL = "https://icanhazdadjoke.com/"
+let responseApi: Joke;
+
+interface Joke {
+    id: string;
+    joke: string;
+    status: number;
+}
+
+function newJoke() {
+    getJoke();
+    showJoke();
+}
+
+async function getJoke(): Promise<Joke> { //para coger los datos de una API
+    const response = await fetch(API_URL, { // fetch conecta con la API (pasamos la url y headers para que me traiga la info en formato json)
+        headers: {
+            Accept: 'application/json',
+        },
+    });
+
+    if (!response.ok) { // si la respuesta es fallida lanza un nuevo error con el estado
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    responseApi = await response.json()
+    return responseApi;
+}
+
+function showJoke() {
+
+    if (responseApi) {
+        const showJokeElement = document.getElementById("showJoke");
+
+        if (showJokeElement) {
+            console.log(responseApi.joke);
+        }
+    }
+
+}
+
